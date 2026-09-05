5097e3e7d020a70bf8af37347001a207342bbbec
CornerDesign-Dominic <dominicfranz4@gmail.com>
2026-09-05 18:28:28 +0200

    Prevent IBAN wrapping in document footer

--- src/components/documents/DocumentShell.jsx
+++ src/components/documents/DocumentShell.jsx
@@ -9,7 +9,7 @@ export default function DocumentShell({ children, label = 'Dokumentvorschau' }) 
         <div><strong>Brennpunkt Logistik GmbH</strong><span>Reinshagenstr. 1</span><span>D-42369 Wuppertal</span></div>
         <div><span><strong>Geschäftsführer:</strong> Dieter Elas</span><span><strong>Handelsregister:</strong> HRB 27075 Wuppertal</span><span><strong>USt-ID-Nr.:</strong> DE304818005</span></div>
         <div><span><strong>Tel:</strong> +49 202 26155-771</span><span><strong>E-Mail:</strong> info@brennpunkt-logistik.de</span><span><strong>Web:</strong> www.brennpunkt-logistik.de</span></div>
-        <div><strong>Stadtsparkasse</strong><span>IBAN: DE16 3405 0000 0000 1318 47</span><span>SWIFT: WELADED1XXX</span></div>
+        <div><strong>Stadtsparkasse</strong><span className="document-shell__iban">IBAN: DE16 3405 0000 0000 1318 47</span><span>SWIFT: WELADED1XXX</span></div>
       </footer>
     </div>
   </article>
